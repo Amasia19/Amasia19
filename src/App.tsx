ff36018a1e4766ff5b62d06327c524df0f7c6d9a
unknown <amasiarajaobelina.jw@gmail.com>
2025-01-20 12:48:00 +0300

    ajout code

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import Login from "./Login";
 import Dashboard from "./Dashboard";
 import "./App.scss";
 import Produit from "./produit";
-
+import Client from "./client";
+import Store from "./store";
 
 function App() {
   return (
@@ -14,7 +15,10 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/produit" element={<Produit />} />
+          <Route path="/client" element={<Client />} />
+          <Route path="/store" element={<Store />} />
           <Route path="/login" element={<Login />} />
+
         </Routes>
       </div>
     </Router>
