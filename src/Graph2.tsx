fb1509ab5fd704df47f8b5cf1eb8a08010035dc7
unknown <amasiarajaobelina.jw@gmail.com>
2025-01-21 09:52:36 +0300

    add color

--- src/Graph2.tsx
+++ src/Graph2.tsx
@@ -2,22 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { PieChart } from '@mui/x-charts/PieChart';
 
 const data = [
-  { label: 'Casques', value: 400 },
-  { label: 'Laptop', value: 300 },
+  { label: 'Casques', value: 400,color: ' #4CAF50' },
+  { label: 'Laptop', value: 300, color: '#d1c40c' },
   { label: 'Phones', value: 500 },
-  { label: 'Sub', value: 200 },
-  { label: 'Ecouteurs', value: 278 },
+  { label: 'Sub', value: 200 ,color: '#40aabd'},
+  { label: 'Ecouteurs', value: 278,color: '#bdbaba' },
 ];
 
 export default function AnimatedPieChart() {
   const [angle, setAngle] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setAngle((prev) => prev + 2); 
-    }, 500); 
-    return () => clearInterval(interval);
-  }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setAngle((prev) => prev + 2); 
+  //   }, 500); 
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return (
     <PieChart
