a463887c42fd998d152adb279167f4c15490fa5a
unknown <amasiarajaobelina.jw@gmail.com>
2025-02-05 08:38:33 +0300

    add code

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,7 +5,7 @@ import "../style/Login.scss";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
- const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = (event: React.FormEvent) => {
@@ -17,6 +17,11 @@ function Login() {
       setIsLoading(false);
       navigate("/dashboard");
     }, 3000);
+  };
+
+  const handleGoogleLogin = () => {
+    setEmail("amasiarajaobelina.jw@gmail.com");
+    setPassword("Amasia*22");
   };
 
   return (
@@ -40,7 +45,12 @@ function Login() {
         <button type="submit" disabled={isLoading}>
           {isLoading ? "Connexion..." : "Se connecter"}
         </button>
-        <button className="google-login" disabled={isLoading}>
+        <button
+          type="button"
+          className="google-login"
+          onClick={handleGoogleLogin}
+          disabled={isLoading}
+        >
           <img src="./src/image/google.svg" alt="Google" className="google" />
           <span className="text">Continue avec Google</span>
         </button>
